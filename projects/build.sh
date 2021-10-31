6888abc4f4f9eac6b61982f6e143b6fa9af5f8ba
arthurnap24 <arthurnap24@gmail.com>
2021-10-31 14:47:37 -0400

    Removed complicated CMake target build cherry-picking, just forward the command line args to cmake --build

--- projects/build.sh
+++ projects/build.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
 set -e
 
-PROJECTS_TO_BUILD=$@
-#TODO: Need a rebuild option
-#if [ -d ./build ]; then
-#    echo "Directory build/ exists, will remove."
-#    rm -rf ./build
-#fi
-
-CMAKE_OPTIONS=""
-for PROJECT in $PROJECTS_TO_BUILD
-do
-    CMAKE_OPTIONS="${CMAKE_OPTIONS}-DBUILD_${PROJECT}=ON "
-done
-
 cmake $CMAKE_OPTIONS . -B ./build
-cmake --build ./build
+cmake --build ./build $@
