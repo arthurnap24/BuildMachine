4e2b882b5300e76b9193b94fb1ad3009a7324ce9
arthurnap24 <arthurnap24@gmail.com>
2022-10-29 07:49:09 -0400

    refactor push_image.sh

--- ci/push_image.sh
+++ ci/push_image.sh
@@ -1,45 +1,85 @@
 #!/bin/bash
 
-function print_usage {
-    echo -e """Usage:
-\tpush_image.sh [alpine|ubuntu|all]
+main() {
+    OS=$1
+    docker_authenticate
+    docker_push $OS
+}
 
-You need to have this environment variables:
-DOCKER_USER=<your Docker account's username>
-DOCKER_PASSWORD=<your Docker account's password>
+docker_authenticate() {
+    check_has_auth_env_vars
+    login
+}
 
-alpine - tags the local alpine-build-machine:latest with \$DOCKER_USER environment variable
-ubuntu - tags the local ubuntu-build-machine:latest with \$DOCKER_USER environment variable
-all - tags both alpine and ubuntu images with \$DOCKER_USER environment variable
+check_has_auth_env_vars() {
+    if [ -z $DOCKER_USER ] || [ -z $DOCKER_PASSWORD ]; then
+        print_usage
+        exit 0;
+    fi
+}
+
+login() {
+    docker login --username $DOCKER_USER --password $DOCKER_PASSWORD
+}
+
+docker_push() {
+    OS=$1
+    if [[ $OS == "alpine" ]]; then
+        push_image_alpine
+    elif [[ $OS == "ubuntu" ]]; then
+        push_image_ubuntu
+    elif [[ $OS == "all" ]]; then
+        push_all_supported_images
+    else
+        not_supported_error
+    fi    
+}
+
+push_image_alpine() {
+    push_image "alpine"
+}
+
+push_image_ubuntu() {
+    push_image "ubuntu"
+}
+
+push_all_supported_images() {
+    push_image_alpine
+    push_image_ubuntu
+}
+
+push_image() {
+    OS=$1
+    IMAGE="${OS}-build-machine:latest"
+    IMAGE_TAG="${DOCKER_USER}/${IMAGE}"
+    docker tag $IMAGE $IMAGE_TAG
+    docker push $IMAGE_TAG
+}
+
+not_supported_error() {
+    print_not_supported
+    exit 1;
+}
+
+print_not_supported() {
+    echo """
+The operating system $1 is not supported, please run with -h flag to view all supported OS.
 """
 }
 
-if [ -z $DOCKER_USER ] || [ -z $DOCKER_PASSWORD ]; then
-    print_usage
-    exit 0;
-fi
+print_usage() {
+    echo -e """Usage:
+\tpush_image.sh <operating-system>
 
-ALPINE_IMAGE="alpine-build-machine:latest"
-UBUNTU_IMAGE="ubuntu-build-machine:latest"
-ALPINE_IMAGE_TAG="${DOCKER_USER}/alpine-build-machine:latest"
-UBUNTU_IMAGE_TAG="${DOCKER_USER}/ubuntu-build-machine:latest"
+Supported operating-system values:
+alpine - tags the local alpine-build-machine:latest
+ubuntu - tags the local ubuntu-build-machine:latest with \$DOCKER_USER environment variable
+all - tags all listed supported images with \$DOCKER_USER environment variable
 
-if [ -z $1 ]; then
-    print_usage
-    exit 0;
-fi
+The Docker commands in this script requires these environment variables:
+DOCKER_USER=<your Docker account's username>
+DOCKER_PASSWORD=<your Docker account's password>
+"""
+}
 
-docker login --username $DOCKER_USER --password $DOCKER_PASSWORD
-
-if [[ $1 == "alpine" ]]; then
-    docker tag $ALPINE_IMAGE $ALPINE_IMAGE_TAG
-    docker push $ALPINE_IMAGE_TAG
-elif [[ $1 == "ubuntu" ]]; then
-    docker tag $UBUNTU_IMAGE $UBUNTU_IMAGE_TAG
-    docker push $UBUNTU_IMAGE_TAG
-elif [[ $1 == "all" ]]; then
-    docker tag $ALPINE_IMAGE $ALPINE_IMAGE_TAG
-    docker tag $UBUNTU_IMAGE $UBUNTU_IMAGE_TAG
-    docker push $ALPINE_IMAGE_TAG
-    docker push $UBUNTU_IMAGE_TAG
-fi
+main $@
